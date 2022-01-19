3a03e345399691c3f1bbe8750d519b13ce620c52
Tyler J. Kenney <tyler@lightmatter.co>
2022-01-19 14:11:22 -0500

    Add Miner class

--- miner.py
+++ miner.py
@@ -32,3 +32,19 @@ class HTTP:
     BadRequest = 400
     NotFound = 404
 # End HTTP
+
+class Miner:
+    '''
+    Miner:
+        ID           <str>: Miner's address used in TKC transactions
+        chain <Blockchain>: Miner's local copy of the blockchain
+        peers        <set>: Set of other active Miners
+    '''
+
+    def __init__(self):
+        self.peers = set()
+        self.ID = str(uuid.uuid4()).replace('-','')
+        self.chain = Blockchain.init()
+
+    def new_transaction(self, sender, receiver, amt):
+        return self.chain.new_transaction(sender,receiver,amt)
