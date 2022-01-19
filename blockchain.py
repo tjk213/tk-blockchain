a5e41701e39515f82ccad97cf995d5b1c4352fd2
Tyler J. Kenney <tyler@lightmatter.co>
2022-01-18 20:22:44 -0500

    [blockchain] Add top-level comment

--- blockchain.py
+++ blockchain.py
@@ -17,6 +17,20 @@
 ####################################################################################
 ####################################################################################
 
+##
+## TK-Blockchain featuring the WegmanHash
+##
+## This file contains an implementation of a simple blockchain using the WegmanHash
+## as the difficult-to-reverse function in the proof-of-work algorithm. It is not
+## at all cryptographically secure, but that's ok because this is only intended for
+## educational purposes.
+##
+## Heavily inspired by Daniel van Flymen's 'Learn Blockchains by Building One'
+## guide on HackerNoon:
+##
+##    https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
+##
+
 import sys
 import time
 
