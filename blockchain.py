83d49d8010de8c2f43bdd36dca3d413774084166
Tyler J. Kenney <tyler@lightmatter.co>
2022-01-18 20:25:45 -0500

    [blockchain] Add Blockchain::valid_chain()

--- blockchain.py
+++ blockchain.py
@@ -302,6 +302,53 @@ class Blockchain:
         hashval = wegman_hash(guess)
         return hashval % 10000000 == 777777 # modulo 10,000 --> reading bottom ~14 bits of hashval
 
+    @staticmethod
+    def valid_chain(chain):
+        '''
+        Params:
+          chain <list>: List of blocks to validate (Blockchain.chain)
+        Returns:
+          Return True if given chain is a valid chain.
+        '''
+
+        # All single-block chains are valid
+        if len(chain) == 1:
+            return True
+
+        ##
+        ## Validate chain
+        ##
+        ##  Each link in the chain is two-fold:
+        ##    - full hash of prior block is stored in current block
+        ##    - proof-of-work is a function of prior proof-of-work
+        ##
+        ##  Why do we need this double-link?
+        ##
+        ##    Well, proof-of-work links are clearly not sufficient by themselves because
+        ##    they *only* link the proofs, so anyone could read a block, save it's proof,
+        ##    update the transactions and the subsequent chain would still pass
+        ##    validation.
+        ##
+        ##    The hash-links solve this problem, but then why do we need the
+        ##    proof-of-work link? Well, I think I'm still a bit fuzzy on this but the
+        ##    proof everyone's competing to find must be a function of some part of the
+        ##    current head, otherwise there'd be no consensus on the problem everyone's
+        ##    trying to solve, or you wouldn't have a chain, or both.
+        ##
+        ##    If valid_proof() was a function of all the transactions in the previous
+        ##    block, as well as it's proof, could you then elminate the hash-link? Yes,
+        ##    I think you could. This would effectively merge the hash-link within the
+        ##    proof-of-work link.
+        ##
+        for i in range(1,len(chain)):
+            block = chain[i]
+            last_block = chain[i-1]
+            if block.prev_hash != hash(last_block) or \
+               not Blockchain.valid_proof(last_block.proof,block.proof):
+                return False
+
+        return True
+
     def create_block(self, proof):
         '''
         Create new block and add it to the chain.
